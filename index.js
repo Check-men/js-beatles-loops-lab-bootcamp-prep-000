761ae06763b3ac70db43082193deabdd4a8d3bd8
Tsuyoshi Akiyama <ka090824@outlook.com>
2017-10-20 04:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,12 @@ function johnLennonFacts(array) {
   }
   return array
 }
+
+function iLoveTheBeatles(n) {
+  var array = []
+
+  do {
+    array.push("I love the Beatles!")
+    n++
+  }while (n < 15)
+}
